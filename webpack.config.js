0f2a4a4a09902c8d6186af98bc04e474b1c4164d
Alex <sasha-elahov@yandex.ru>
2020-06-25 19:49:07 +0300

    setup project

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+const config = {
+  entry: ["react-hot-loader/patch", "./src/index.js"],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+    alias: {
+      "react-dom": "@hot-loader/react-dom",
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      appMountId: "app",
+      filename: "index.html",
+    }),
+    new CopyPlugin([{ from: "src/index.html" }]),
+    new MiniCssExtractPlugin(),
+    new CleanWebpackPlugin(),
+  ],
+  devServer: {
+    contentBase: "./dist",
+  },
+};
+
+module.exports = config;
